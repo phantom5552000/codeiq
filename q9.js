30e34c874878070c13e8a4a8609634e215dd3605
phantom5552000 <nakagawa.isamu@gmail.com>
2018-10-07 18:10:55 +0900

    遅すぎて終わらない

--- q9.js
+++ q9.js
@@ -12,28 +12,55 @@
         }
         return true;
     };
-    var perm_1 = function (remain, result) {
-        console.log("remain: ", remain);
-        console.log("result: ", result);
+    var sameNumber_1 = function (l) {
+        if (l.length == 0)
+            return false;
+        if (l.length % 2 == 1) {
+            return false;
+        }
+        return l.length / 2 == R_1.sum(l);
+    };
+    var perm_1 = function (remain, result, found) {
+        //console.log("remain: ", remain);
+        //console.log("  result: ", result);
+        //console.log(`count: ${count}`);
+        if (sameNumber_1(result)) {
+            //console.log(`${result}, sum=${R.sum(result)}, ${sameNumber(result)}`);
+            return 0;
+        }
         if (isAllEmpty_1(remain)) {
-            console.log(result);
-            return true;
+            if (found[0] % 1000 == 0)
+                console.log(result + ", sum=" + R_1.sum(result) + ", " + sameNumber_1(result));
+            //console.log("*** ",result, R.sum(result), sameNumber(result));
+            found[0]++;
         }
         for (var i = 0; i < remain.length; i++) {
-            console.log("---%d", i);
-            var remain_copy = remain.slice();
-            var res_copy = result.slice();
+            //console.log("---%d", i);
+            var remain_copy = JSON.parse(JSON.stringify(remain)); // deep copy
             if (remain_copy[i].length == 0)
                 continue;
+            var res_copy = JSON.parse(JSON.stringify(result));
             var first = R_1.nth(0, remain_copy[i]);
             remain_copy[i].pop();
             res_copy.push(first);
-            //console.log("remain_copy: ", remain_copy);
-            //console.log("res_copy: ", res_copy);
-            perm_1(remain_copy.slice(), res_copy.slice());
+            //console.log("  remain_copy: ", remain_copy);
+            //console.log("   res_copy: ", res_copy);
+            perm_1(remain_copy.slice(), res_copy.slice(), found);
         }
-        //=> [1, 2, 3]
-        //console.log(x);
     };
-    perm_1(all, result); //=> [1, 2, 3]
+    var makeCopy = function (obj, times) {
+        var l = [];
+        for (var i = 0; i < times; i++) {
+            l.push(obj);
+        }
+        return l;
+    };
+    var count = 0;
+    var found = [0];
+    perm_1([makeCopy(0, 3), makeCopy(1, 2)], [], found); //=> [1, 2, 3]
+    console.log(found);
+    found[0] = 0;
+    perm_1([makeCopy(0, 30), makeCopy(1, 20)], [], found); //=> [1, 2, 3]
+    console.log(found);
+    //perm([makeCopy(0,15), makeCopy(1,10)], result); //=> [1, 2, 3]
 }
--- q9.ts
+++ q9.ts
@@ -14,33 +14,56 @@
         return true;
     }
 
-    let perm = (remain:Array<Array<number>>, result:Array<number>) => {
-        console.log("remain: ", remain);
-        console.log("result: ", result);
-        if(isAllEmpty(remain)){
-            console.log(result);
-            return true;
+    let sameNumber = (l:Array<number>):boolean => {
+        if(l.length == 0) return false;
+        if(l.length % 2 ==1){
+            return false;
         }
+        return l.length/2 == R.sum(l);
+    }
+    let perm = (remain:Array<Array<number>>, result:Array<number>, found:Array<number>) => {
+        //console.log("remain: ", remain);
+        //console.log("  result: ", result);
+        //console.log(`count: ${count}`);
+        if(sameNumber(result)){
+            //console.log(`${result}, sum=${R.sum(result)}, ${sameNumber(result)}`);
+            return 0;
+        }
+        if(isAllEmpty(remain)){
+            if(found[0]%1000==0)console.log(`${result}, sum=${R.sum(result)}, ${sameNumber(result)}`);
+             //console.log("*** ",result, R.sum(result), sameNumber(result));
+            found[0]++;
+            }
         
-        let res_copy = result.slice();
         for(let i=0;i<remain.length;i++){
-            console.log("---%d", i);
-            let remain_copy = remain.slice(); // deep copy
+            //console.log("---%d", i);
+            let remain_copy = JSON.parse(JSON.stringify(remain)); // deep copy
             if(remain_copy[i].length == 0)continue;
-
+            let res_copy = JSON.parse(JSON.stringify(result));
             let first = R.nth(0,remain_copy[i]);
             remain_copy[i].pop();
             res_copy.push(first);
-            //console.log("remain_copy: ", remain_copy);
-            //console.log("res_copy: ", res_copy);
+            //console.log("  remain_copy: ", remain_copy);
+            //console.log("   res_copy: ", res_copy);
     
-            perm(remain_copy.slice(), res_copy.slice());
-
+            perm(remain_copy.slice(), res_copy.slice(), found);
         }
-        
-        //=> [1, 2, 3]
-        //console.log(x);
     }
-    perm(all, result); //=> [1, 2, 3]
+    let    makeCopy = (obj:any, times:number) => {
+        let l = [];
+        for(let i=0;i<times;i++){
+            l.push(obj);
+        }
+        return l;
+    }
+    let count = 0;
+    let found:Array<number> =[0];
+    perm([makeCopy(0,3), makeCopy(1,2)], [] ,found); //=> [1, 2, 3]
+    console.log(found);
+    
+    found[0] = 0;
+    perm([makeCopy(0,30), makeCopy(1,20)], [] ,found); //=> [1, 2, 3]
+    console.log(found);
+    //perm([makeCopy(0,15), makeCopy(1,10)], result); //=> [1, 2, 3]
 
 }
