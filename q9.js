616db5aee3cf874b1d9fa77273c428bf9f64b64b
phantom5552000 <nakagawa.isamu@gmail.com>
2018-10-08 14:11:13 +0900

    -

--- q9.js
+++ q9.js
@@ -1,43 +1,70 @@
+"use strict";
+exports.__esModule = true;
 {
-    var R_1 = require('ramda');
+    var R = require('ramda');
     var printf = require('printf');
     var _a = require('perf_hooks'), PerformanceObserver_1 = _a.PerformanceObserver, performance_1 = _a.performance;
-    var number_male = 20; //parseInt(process.argv[2]);
-    var number_female = 10; //parseInt(process.argv[3]);
-    var count_f_1 = 0;
-    var factorial_1 = R_1.memoize(function (n) {
-        count_f_1 += 1;
-        return R_1.product(R_1.range(1, n + 1));
-    });
-    var combi = function (q, r) {
-        var p = q + r;
-        var loop = p - q;
-        var prd = 1;
-        for (var i = 0; i < loop; i++) {
-            prd = prd * (p - i);
-        }
-        var b = factorial_1(r);
-        return prd / b;
-    };
-    var count = 0;
-    var start = performance_1.now();
-    console.log("factorial(" + number_male + ") = " + factorial_1(number_male));
-    console.log("factorial(" + number_female + ") = " + factorial_1(number_female));
-    //perm([makeCopy(0,number_male), makeCopy(1,number_female)], [] ,found); //=> [1, 2, 3]
-    var c = combi(number_male, number_female);
-    console.log("n: " + number_male + ", " + number_female + ", combi: " + c);
-    var sub_list = [];
-    for (var m = number_male - 1, f = number_female - 1; f >= 0; m--, f--) {
-        var c1 = combi(m, f);
-        sub_list.push(c1);
-        console.log("  n: " + m + ", " + f + ", combi: " + c1);
+    var boys = 2; //parseInt(process.argv[2]);
+    var girls = 1; //parseInt(process.argv[3]);
+    if (process.argv.length > 3) {
+        boys = parseInt(process.argv[2]);
+        girls = parseInt(process.argv[3]);
     }
+    var Position = /** @class */ (function () {
+        function Position(x, y) {
+            this.x = x;
+            this.y = y;
+            this.v = 0;
+        }
+        return Position;
+    }());
+    ;
+    var cnt = 1;
+    var m_1 = new Array(girls + 2);
+    for (var i = 0; i < girls + 2; i++) {
+        m_1[i] = new Array(boys + 2); // （2）
+        for (var j = 0; j < boys + 2; j++) {
+            m_1[i][j] = cnt++;
+            if (i == 0 || j == 0) {
+                m_1[i][j] = 0;
+            }
+        }
+    }
+    m_1[1][1] = 1;
+    var route = function (boys, girls) {
+        for (var i_1 = 1; i_1 < girls + 2; i_1++) {
+            for (var j = 1; j < boys + 2; j++) {
+                //console.log(i,j);
+                if (i_1 == 1 && j == 1)
+                    continue;
+                if (i_1 == j || boys - j == girls - i_1) {
+                    m_1[i_1][j] = 0;
+                }
+                else {
+                    m_1[i_1][j] = m_1[i_1 - 1][j] + m_1[i_1][j - 1];
+                }
+            }
+        }
+    };
+    var print_route = function () {
+        for (var i_2 = 0; i_2 < m_1.length; i_2++) {
+            var line = "";
+            for (var j = 0; j < m_1[i_2].length; j++) {
+                line += m_1[i_2][j].toString();
+                line += " ";
+            }
+            console.log(line);
+        }
+    };
+    console.log("boys:" + boys + ", girls:" + girls);
+    var start = performance_1.now();
+    route(boys, girls);
     var end = performance_1.now();
-    //console.log(found);
+    //console.log(found);    
     var elapsed = (end - start);
     var elapsedStr = elapsed.toPrecision(3);
+    var ans = m_1[girls + 1][boys - 1] + m_1[girls - 1][boys + 1];
     //console.log("result.len=%d", result.length);
-    console.log("n: " + number_male + ", " + number_female + ", combi: " + c + "  " + elapsedStr + " mSec");
-    var a = c - R_1.sum(sub_list);
-    console.log(a);
+    print_route();
+    console.log(boys + ", " + girls + ", combi: " + ans + "  " + elapsedStr + " mSec");
 }
--- q9.ts
+++ q9.ts
@@ -1,52 +1,72 @@
+import { pipeline } from "stream";
+
 {
     const R = require('ramda');
     const printf = require('printf');
     const { PerformanceObserver, performance } = require('perf_hooks');
 
-    let number_male = 20;//parseInt(process.argv[2]);
-    let number_female = 10;//parseInt(process.argv[3]);
-    let count_f = 0;
-    const factorial = R.memoize((n:number) => {
-        count_f += 1;
-        return R.product(R.range(1, n + 1));
-        }
-    );
-
-    let combi = (q:number, r:number) =>{
-        let p = q+r;
-        let loop = p - q;
-        let prd = 1;
-        for(let i=0; i<loop; i++){
-            prd = prd * (p - i);
-        }
-        let b = factorial(r);
-        return prd/b;
-    }
-    let count = 0;
+    let boys  = 2;//parseInt(process.argv[2]);
+    let girls = 1;//parseInt(process.argv[3]);
+    if(process.argv.length > 3){
+        boys  = parseInt(process.argv[2]);
+        girls = parseInt(process.argv[3]);
     
+    }
+    class Position{
+        x: number;
+        y: number;
+        v: number;
+        constructor(x:number, y:number) {
+            this.x = x;
+            this.y = y;
+            this.v = 0;
+        }
+    };
+    let cnt = 1;
+    let m: Array<number>[] = new Array(girls+2);
+    for ( var i = 0; i < girls+2; i++ ) {
+        m[i] = new Array(boys+2);  // （2）
+        for(let j=0;j<boys+2;j++){
+            m[i][j] = cnt++;
+            if(i==0 || j==0){
+                m[i][j] = 0;    
+            }
+        }
+    }
+    m[1][1] = 1;
+    let route = (boys:number, girls:number) =>{
+        for(let i=1;i<girls+2;i++){
+            for(let j=1;j<boys+2;j++){
+                //console.log(i,j);
+                if(i==1 && j==1)    continue;
+                if(i==j || boys-j==girls-i){
+                    m[i][j] = 0;
+                }else{
+                    m[i][j] = m[i-1][j] + m[i][j-1];
+                }
+            }
+        }
+    }
+    let print_route = () =>{
+        for(let i=0;i<m.length;i++){
+            let line = "";
+            for(let j=0;j<m[i].length;j++){
+                line += m[i][j].toString();
+                line += " "
+            }
+            console.log(line);
+        }
+
+    }
+    console.log(`boys:${boys}, girls:${girls}`);
     const start = performance.now();
-    console.log(`factorial(${number_male}) = ${factorial(number_male)}`);
-    console.log(`factorial(${number_female}) = ${factorial(number_female)}`);
-    //perm([makeCopy(0,number_male), makeCopy(1,number_female)], [] ,found); //=> [1, 2, 3]
-    let c = combi(number_male, number_female);
-    console.log(`n: ${number_male}, ${number_female}, combi: ${c}`);
-
-    let sub_list = [];
-    for(let m=number_male-1, f=number_female-1; f>=0;m--, f--){
-        let c1 = combi(m, f);
-        sub_list.push(c1);
-        console.log(`  n: ${m}, ${f}, combi: ${c1}`);
-    
-    }
-
+    route(boys,girls);
     const end = performance.now();
-    //console.log(found);
-    
+    //console.log(found);    
     const elapsed = (end - start);
     const elapsedStr = elapsed.toPrecision(3);
-    
+    let ans = m[girls+1][boys-1] + m[girls-1][boys+1];
     //console.log("result.len=%d", result.length);
-    console.log(`n: ${number_male}, ${number_female}, combi: ${c}  ${elapsedStr} mSec`);
-    let a = c - R.sum(sub_list);
-    console.log(a);
+    print_route();
+    console.log(`${boys}, ${girls}, combi: ${ans}  ${elapsedStr} mSec`);
 }
