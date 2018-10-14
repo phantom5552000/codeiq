9d432ef63c8652da2ed5e84ebe6f74e6be3dca5b
phantom5552000 <nakagawa.isamu@gmail.com>
2018-10-14 18:54:31 +0900

    -

--- q13.ts
+++ q13.ts
@@ -27,28 +27,44 @@
             console.log(d);
         }, R.range(0,10));
     };*/
-    
-    let check_combi = () =>{
-        let d,e,k:number;
-        R.forEach((d:number) => {
-            R.forEach((e:number) => {
-                R.forEach((k:number) => {
-                    R.forEach((l:number) => {
+    let r:number,e:number,a:number,d:number, w:number, i:number, t:number, l:number, k:number, s:number;
+    let meat = () => {
+        let read = r*1000+e*100+a*10+d;
+        let write = w*10000+r*1000+i*100+t*10+e;
+        let talk = t*1000+a*100+l*10+k;
+        let skill = s*10000+k*1000+i*100+l*10+l;
 
-                        console.log(d*1000+e*100+k*10+l);
-                    }, R.range(0,10))
-                }, R.range(0,10))
-            }, R.range(0,10))
-        }, R.range(0,10));
+        return (read+write+talk) == skill;
+
+    }
+    let cc = 0;
+    let check_combi = (ee:Array<number>, count:number, rr:Array<number>):Array<number> =>{
+        // リストから一つ取り出す
+        if(rr.length == count) {
+            if((rr[0]+rr[1]+rr[2]) % 10 == rr[3]){
+                cc++;
+                console.log(rr);
+            }
+            return [];
+        }
+        for(let i=0;i<ee.length;i++){
+            let nth = R.nth(i, ee);
+            let remain = R.remove(i, 1, ee);
+            let rrr = R.clone(rr);
+            //console.log(`${nth}, ${remain}`);
+            rrr.push(nth);
+            check_combi(remain, count, rrr);
+        }
+        return [];
     }
 
     const start = performance.now();
-    check_combi();
+    check_combi(R.range(0,10), 4, []);
 
     const end = performance.now();
     //console.log(found);    
     const elapsed = (end - start);
     const elapsedStr = elapsed.toPrecision(3);
-    console.log(`index:${n}, ${elapsedStr} mSec`);
+    console.log(`index:${cc}, ${elapsedStr} mSec`);
 
 }
